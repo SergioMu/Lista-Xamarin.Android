7215452229ed23f6193d61be02ba04afe83c1ca2
SergioMu <Sergio_mu@outlook.com>
2015-03-27 20:20:46 +0100

    Primera subida

---
+++ TutorialListView/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace TutorialListView
+{
+	[Activity (Label = "TutorialListView", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		private List<string> misElementos;
+		private ListView misSeries;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			misSeries = FindViewById<ListView> (Resource.Id.miLista);
+
+			misElementos = new List<string>();
+			misElementos.Add ("The Walking Dead");
+			misElementos.Add ("El Mentalista");
+			misElementos.Add ("Big Bang");
+			misElementos.Add ("New Girl");
+
+			ArrayAdapter<String> adaptador = new ArrayAdapter<string>(this, 
+				Android.Resource.Layout.SimpleListItem1, misElementos);
+
+			misSeries.Adapter = adaptador;
+
+		}
+	}
+}
